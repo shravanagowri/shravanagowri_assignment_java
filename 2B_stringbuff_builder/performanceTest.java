8b7ca0496ce21a2bf6220318936dca3448ceed6b
Shravana Gowri <110216184+shravanagowri@users.noreply.github.com>
2025-05-22 20:02:09 +0530

    Add files via upload

---
+++ 2B_stringbuff_builder/performanceTest.java
@@ -0,0 +1,38 @@
+package ass2b;
+
+public class performanceTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int iterations = 10000;
+        String appendStr = "AIET";
+
+        // Test with StringBuffer
+        StringBuffer sbuffer = new StringBuffer();
+        long startBuffer = System.nanoTime();
+        for (int i = 0; i < iterations; i++) {
+            sbuffer.append(appendStr);
+        }
+        long endBuffer = System.nanoTime();
+        long timeBuffer = endBuffer - startBuffer;
+        System.out.println("Time taken by StringBuffer: " + timeBuffer + " ns");
+
+        // Test with StringBuilder
+        StringBuilder sbuilder = new StringBuilder();
+        long startBuilder = System.nanoTime();
+        for (int i = 0; i < iterations; i++) {
+            sbuilder.append(appendStr);
+        }
+        long endBuilder = System.nanoTime();
+        long timeBuilder = endBuilder - startBuilder;
+        System.out.println("Time taken by StringBuilder: " + timeBuilder + " ns");
+
+        // Conclusion
+        if (timeBuilder < timeBuffer) {
+            System.out.println("➡ StringBuilder is faster than StringBuffer.");
+        } else {
+            System.out.println("➡ StringBuffer is faster than StringBuilder.");
+        }
+	}
+
+}
