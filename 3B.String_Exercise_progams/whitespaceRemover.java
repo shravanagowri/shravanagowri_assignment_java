7e87f141c4df3014fc113af3b44c202c062cc9d7
Shravana Gowri <110216184+shravanagowri@users.noreply.github.com>
2025-05-22 21:45:06 +0530

    Add files via upload

---
+++ 3B.String_Exercise_progams/whitespaceRemover.java
@@ -0,0 +1,24 @@
+package ass3b5;
+import java.util.Scanner;
+public class whitespaceRemover {
+	// User-defined function to remove all whitespace characters
+    public static String removeWhitespace(String str) {
+        if (str == null) return null;
+
+        // Use regex to remove all types of whitespace (spaces, tabs, newlines, etc.)
+        return str.replaceAll("\\s+", "");
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string with whitespaces: ");
+        String input = scanner.nextLine();
+
+        String result = removeWhitespace(input);
+        System.out.println("String after removing whitespaces: " + result);
+
+        scanner.close();
+	}
+
+}
