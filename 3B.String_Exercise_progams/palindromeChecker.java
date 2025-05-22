ad59b14102a4b5414102e88643b5240c898d4e2a
Shravana Gowri <110216184+shravanagowri@users.noreply.github.com>
2025-05-22 21:40:34 +0530

    palindromeChecker.java

--- 3B.String_Exercise_progams/palindromeChecker.java
+++ 3B.String_Exercise_progams/palindromeChecker.java
@@ -1,3 +1,5 @@
+*3B4:Write a Java Program for Checking if a string reads the same backward as forward (ignoring case
+and punctuation) using user defined function isPalindrome():*/
 package ass3b4;
 import java.util.Scanner;
 public class palindromeChecker {
