79f885a8093886497746c548d70cd77b32d9dfc5
Shravana Gowri <110216184+shravanagowri@users.noreply.github.com>
2025-05-22 19:47:12 +0530

    Add files via upload

---
+++ 1C_linkedlist.java/DisplayElementsWithPositions.java
@@ -0,0 +1,22 @@
+package ass1C4;
+import java.util.LinkedList;
+public class DisplayElementsWithPositions {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList<String> l_list = new LinkedList<>();
+
+        // Add elements
+        l_list.add("Red");
+        l_list.add("Green");
+        l_list.add("Blue");
+        l_list.add("Yellow");
+        l_list.add("Orange");
+
+        // Display elements with their positions
+        System.out.println("Elements and their positions in the LinkedList:");
+        for (int i = 0; i < l_list.size(); i++) {
+            System.out.println("Position " + i + ": " + l_list.get(i));
+        }
+	}
+
+}
