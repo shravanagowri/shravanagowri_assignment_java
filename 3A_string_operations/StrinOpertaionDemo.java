97c12b577a3c27bedc24c09710a7a0c6801dfd1f
Shravana Gowri <110216184+shravanagowri@users.noreply.github.com>
2025-05-22 21:11:33 +0530

    StrinOpertaionDemo.java

--- 3A_string_operations/StrinOpertaionDemo.java
+++ 3A_string_operations/StrinOpertaionDemo.java
@@ -1,3 +1,10 @@
+*/3A:Develop a java program for performing various string operations with different string
+handling functions directed as follows:
+String Creation and Basic Operations, Length and Character Access, String Comparison, String
+Searching, Substring Operations , String Modification, Whitespace Handling, String Concatenation,
+String Splitting, StringBuilder Demo, String Formatting , Validate Email with contains( ) and
+startsWith() and endsWith()*/
+	
 package ass3a;
 public class StrinOpertaionDemo {
 	public static void main(String[] args) {
