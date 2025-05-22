b7740e83bcf2ea546b2c0b28afd5f6de3b119419
Shravana Gowri <110216184+shravanagowri@users.noreply.github.com>
2025-05-22 19:01:05 +0530

    ListOperationDemo.java

--- 1A_arraylist/ListOperationDemo.java
+++ 1A_arraylist/ListOperationDemo.java
@@ -1,3 +1,10 @@
+*/1A.Develop a Java program for adding elements [Apple, Banana, Orange] into an ArrayList
+and a LinkedList to perform the following operations with different functions directed as
+follows
+1. Adding elements, 2. Adding element at specific index, 3. Adding multiple elements, 4.
+Accessing elements,5. Updating elements, 6. Removing elements, 7. Searching elements, 8. List
+size, 9. Iterating over list, 10. Using Iterator, 11. Sorting, 12. Sublist, 13. Clearing the list*/
+    
 package ass1a;
 
 import java.util.*;
