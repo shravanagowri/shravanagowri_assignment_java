fad4a787408dcd69dd39df517c686cf409df52ac
Shravana Gowri <110216184+shravanagowri@users.noreply.github.com>
2025-05-22 19:33:28 +0530

    Add files via upload

---
+++ 1C_linkedlist.java/IterateFromSecond.java
@@ -0,0 +1,31 @@
+package ass1C1;
+import java.util.LinkedList;
+import java.util.ListIterator;
+public class IterateFromSecond {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList<String> colors = new LinkedList<>();
+
+        // Add elements
+        colors.add("Red");     // index 0
+        colors.add("Green");   // index 1
+        colors.add("Blue");    // index 2
+        colors.add("Yellow");  // index 3
+        colors.add("Orange");  // index 4
+
+        // Display the full list
+        System.out.println("Full color list: " + colors);
+
+        // Create a ListIterator starting at index 1 (2nd position)
+        ListIterator<String> iterator = colors.listIterator(1);
+
+        // Iterate from the 2nd element onwards
+        System.out.println("Iterating from 2nd position:");
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
+	}
+
+}
