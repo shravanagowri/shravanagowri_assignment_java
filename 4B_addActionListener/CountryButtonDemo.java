01ae631fcdb3ef9cc78f49cb1d351aebe19582e2
Shravana Gowri <110216184+shravanagowri@users.noreply.github.com>
2025-05-22 22:22:46 +0530

    CountryButtonDemo.java

--- 4B_addActionListener/CountryButtonDemo.java
+++ 4B_addActionListener/CountryButtonDemo.java
@@ -1,3 +1,6 @@
+*/4B.Develop a Swing program in Java to display a message “Srilanka is pressed” or “India is
+pressed” depending upon the Jbutton either Srilanka or India is pressed by implementing the
+event handling mechanism with addActionListener( ).*/
 package ass4b;
 import javax.swing.*;
 import java.awt.*;
