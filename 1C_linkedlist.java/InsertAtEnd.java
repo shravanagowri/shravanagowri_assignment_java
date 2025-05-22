0615049483957b4f8f0701a322579157863bb249
Shravana Gowri <110216184+shravanagowri@users.noreply.github.com>
2025-05-22 19:41:27 +0530

    Add files via upload

---
+++ 1C_linkedlist.java/InsertAtEnd.java
@@ -0,0 +1,23 @@
+package ass1C3;
+import java.util.LinkedList;
+public class InsertAtEnd {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList<String> l_list = new LinkedList<>();
+
+        // Add initial elements
+        l_list.add("Red");
+        l_list.add("Green");
+        l_list.add("Blue");
+
+        // Display original list
+        System.out.println("Original LinkedList: " + l_list);
+
+        // Insert "Pink" at the end using offerLast()
+        l_list.offerLast("Pink");
+
+        // Display updated list
+        System.out.println("LinkedList after inserting 'Pink' at the end: " + l_list);
+	}
+
+}
