26c4ecef59c704db7b1191697bc3561c760b4766
Shravana Gowri <110216184+shravanagowri@users.noreply.github.com>
2025-05-22 19:37:00 +0530

    IterateFromSecond.java

--- 1C_linkedlist.java/IterateFromSecond.java
+++ 1C_linkedlist.java/IterateFromSecond.java
@@ -1,3 +1,5 @@
+*/1C1.Write a Java program to iterate through all elements in a linked list starting at the
+specified position ( 2nd) using iterator ( hint : Iterator iteratorobj = listobj.listIterator(1))*/
 package ass1C1;
 import java.util.LinkedList;
 import java.util.ListIterator;
