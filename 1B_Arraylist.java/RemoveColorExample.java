3adeb5b99c0cb02afcd2dbf67bf062ac6f8154b9
Shravana Gowri <110216184+shravanagowri@users.noreply.github.com>
2025-05-22 19:10:18 +0530

    Add files via upload

---
+++ 1B_Arraylist.java/RemoveColorExample.java
@@ -0,0 +1,37 @@
+package ass1b2;
+import java.util.ArrayList;
+public class RemoveColorExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String> colors = new ArrayList<>();
+
+        // Add some colors
+        colors.add("Red");
+        colors.add("Green");
+        colors.add("Blue");
+        colors.add("Yellow");
+        colors.add("Purple");
+
+        // Display original list
+        System.out.println("Original color list: " + colors);
+
+        // Remove the 2nd element (index 1)
+        if (colors.size() > 1) {
+            String removedByIndex = colors.remove(1); // Removes "Green"
+            System.out.println("Removed 2nd element: " + removedByIndex);
+        }
+
+        // Remove the color "Blue" by value
+        boolean isBlueRemoved = colors.remove("Blue");
+        if (isBlueRemoved) {
+            System.out.println("Color 'Blue' removed from the list.");
+        } else {
+            System.out.println("Color 'Blue' was not found in the list.");
+        }
+
+        // Display updated list
+        System.out.println("Updated color list: " + colors);
+	}
+
+}
